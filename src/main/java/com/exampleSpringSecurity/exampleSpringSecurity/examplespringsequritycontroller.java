09861245680dd2e3f69a6421df2fd95f5afdc7c3
nambi45 <nambiraj003@gmail.com>
2022-07-06 07:57:48 +0530

    First Commit

---
+++ src/main/java/com/exampleSpringSecurity/exampleSpringSecurity/examplespringsequritycontroller.java
@@ -0,0 +1,14 @@
+package com.exampleSpringSecurity.exampleSpringSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class examplespringsequritycontroller {
+	
+	@GetMapping("/springsecurity")
+	public String springSecurity(){
+		return "SpringSecurity";
+	}
+
+}
---
+++ src/test/java/com/exampleSpringSecurity/exampleSpringSecurity/ExampleSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleSpringSecurity.exampleSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
